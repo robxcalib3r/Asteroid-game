5ce5526f4c6546126f424ef263d4ef627a77b850
Raven <robxcalib3r@gmail.com>
2024-08-23 14:45:35 +0600

    add delta time

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, BLACK_COLOR)
         pygame.display.flip()
+
+    # limit the framerate to 60
+    dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
